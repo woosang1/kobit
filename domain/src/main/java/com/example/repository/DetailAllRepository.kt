27a3542e05fe06eade57f56dcb68372317d1ff4e
woosang <wsl2918@nolbal.com>
2023-05-20 10:01:46 +0900

    style : 오타수정

--- domain/src/main/java/com/example/repository/DetailAllRepository.kt
+++ domain/src/main/java/com/example/repository/DetailAllRepository.kt
@@ -3,5 +3,5 @@ package com.example.repository
 import com.example.model.CoinListModel
 
 interface DetailAllRepository {
-    fun :getDetailAll(success: (CoinListModel) -> Unit, fail: (String) -> Unit)
+    fun getDetailAll(success: (CoinListModel) -> Unit, fail: (String) -> Unit)
 }
